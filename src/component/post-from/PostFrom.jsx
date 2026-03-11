55ec33050f3ea1581c230f2148e2228f14676c7d
Nabil Ahmad Rifat <106903272+Nabil-Ahamad-Rifat@users.noreply.github.com>
2026-03-11 23:50:03 +0600

    refactor: enhance PostFrom component with improved image handling and slug transformation

--- src/component/post-from/PostFrom.jsx
+++ src/component/post-from/PostFrom.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { useForm } from "react-hook-form";
+import React, { useCallback } from "react";
+import { useForm, Watch } from "react-hook-form";
 import { Button, Input, Select, Rte } from "../index";
 import service from "../../appWrite/configAp";
 import { useNavigate } from "react-router-dom";
@@ -8,23 +8,124 @@ import { useSelector } from "react-redux";
 function PostFrom(post) {
   const { register, handlSubmit, watch, setValue, control, getValues } =
     useForm({
-        defaultValues: { title: post?.title || "",
-        slug: post?.slug || "" },
-        content:post.content ||"",
-        status:post?.status ||'active',
+      defaultValues: { title: post?.title || "", slug: post?.slug || "" },
+      content: post.content || "",
+      status: post?.status || "active",
     });
-    const navigate = useNavigate();
-    const userData = useSelector(state => state.auth.userData)
-    const submit = async(data)=>{
-        if(post){
-            data.image[0] ? service.uploadfile :null
-        }
-    } 
+  const navigate = useNavigate();
+  const userData = useSelector((state) => state.auth.userData);
+  const submit = async (data) => {
+    if (post) {
+      const file = data.image[0] ? service.uploadImage(data.image[0]) : null;
+      if (file) {
+        service.delefile(post.featuredImage);
+      }
 
+      const bdPost = await service.updatePost(post.$id, {
+        ...data,
+        featuredImage: file ? file.$id : undefined,
+      });
+      if (bdPost) {
+        navigate(`/post/${bdPost.$id}`);
+      }
+    } else {
+      const file = await service.uploadImage(data.image[0]);
+    }
+    if (file) {
+      const fileId = file.$id;
+      data.featuredImage = fileId;
+      const dbpost = await service.createPost({
+        ...data,
+        userId: userData.$id,
+      });
+      if (dbpost) {
+        navigate(`/post/${dbpost.$id}`);
+      }
+    }
+  };
+  const slugTransform = useCallback((value) => {
+    if (value && typeof value === "string") {
+      return value
+        .toLowerCase()
+        .trim()
+        .replace(/[\s_]+/g, "-");
+    }
+    return "";
+  }, []);
 
+  const subscription = Watch(
+    (value, { name }) => {
+      if (name === "title") {
+        setValue("slug", slugTransform(value.title, { shouldValidate: true }));
+      }
+      return () => {
+        subscription.unsubscribe();
+      };
+    },
+    [watch, slugTransform, setValue],
+  );
 
+  return (
+    <div>
+      <form
+        action=""
+        onSubmit={handlSubmit(submit)}
+        className="flex flex-wrap "
+      >
+        <div className="w-2/3 px-2">
+          <input
+            label="title:"
+            placeholder="title"
+            className="mb-4"
+            {...register(title, { required: true })}
+          />
+          <Input
+            label="Slug:"
+            placeholder="slug"
+            className="mb-4"
+            {...register("slug", { required: true })}
+            onInput={(e) => {
+              setValue("slug", slugTransform(e.currentTarget.value), {
+                shouldValidate: true,
+              });
+            }}
+          />
 
-  return <div>PostFrom</div>;
+          <Rte
+            label="content:"
+            name="content"
+            control={control}
+            defaultValue={getValues("content")}
+          />
+        </div>
+        <div className=" w-1/3 px-2">
+          <Input
+            label="FeatureImage"
+            type="file"
+            className="mb-4"
+            accept="image/png, image/jpg, image/gif"
+            {...register("image", { required: !post })}
+          />
+          {post && (
+            <div className="w-full mb-4 ">
+              <img
+                src={service.getfilePreview(post.featureImage)}
+                alt="post.title"
+                className="rounded-lg"
+              />
+            </div>
+          )}
+          <Select
+          options= {["active","inactive"]}
+          label="status"
+          className="mb-4"
+          {...register("status",{required:true})}
+          />
+          <Button type="submit" bgColor={`post ? "bg-green-500":undefine`} className="w-full"> {post ? "update" :"submit"}</Button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default PostFrom;
